f1411e923d057c86bea51e79ef9eff416ebc9515
joestephens <joe.stephens@live.co.uk>
2018-03-14 20:41:24 +0000

    Add readCurrentQuestion to Quiz

--- __tests__/quiz.test.js
+++ __tests__/quiz.test.js
@@ -7,3 +7,13 @@ describe('Quiz constructor', () => {
     expect(quiz.questions).toEqual(questions);
   });
 });
+
+describe('readCurrentQuestion', () => {
+  it('returns the challenge of the current question', () => {
+    const mockChallenge = jest.fn();
+    const questions = [{ challenge: mockChallenge }];
+    const quiz = new Quiz(questions);
+
+    expect(quiz.readCurrentQuestion()).toEqual(mockChallenge);
+  });
+});
--- src/quiz.js
+++ src/quiz.js
@@ -2,4 +2,8 @@ function Quiz(questions) {
   this.questions = questions;
 }
 
+Quiz.prototype.readCurrentQuestion = function readCurrentQuestion() {
+  return this.questions[0].challenge;
+}
+
 module.exports = Quiz;
