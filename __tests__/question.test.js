30d744b98601ca1c154db9b0c40f0244b4feccc0
Michael Harrison <michael.harrison@peak.ai>
2018-09-26 20:29:08 +0100

    Refactor tests

--- __tests__/question.test.js
+++ __tests__/question.test.js
@@ -1,35 +1,40 @@
 const Question = require('../src/question');
 
+let question = null;
+
+beforeEach(() => {
+  question = new Question('What is 2 + 2?', '4');
+});
+
+afterEach(() => {
+  question = null;
+});
+
 describe('Question', () => {
   it('has the given challenge property', () => {
-    const question = new Question('What is 2 + 2?', '4');
     expect(question.challenge).toEqual('What is 2 + 2?');
   });
 
   it('has the given answer property', () => {
-    const question = new Question('What is 2 + 2?', '4');
     expect(question.answer).toEqual('4');
   });
 });
 
 describe('checkAnswer', () => {
   it('returns false if the guess is wrong', () => {
-    const question = new Question('What is 2 + 2?', '4');
     expect(question.checkAnswer('3')).toBe(false);
   });
 
   it('returns true if the guess is correct', () => {
-    const question = new Question('What is 2 + 2?', '4');
     expect(question.checkAnswer('4')).toBe(true);
   });
 
   it('returns true if the guess is the correct number', () => {
-    const question = new Question('What is 2 + 2?', '4');
     expect(question.checkAnswer(4)).toBe(true);
   });
 
   it('does a case-insensitive comparison', () => {
-    const question = new Question('What is 2 + 2?', 'FoUr');
+    question = new Question('What is 2 + 2?', 'FoUr');
     expect(question.checkAnswer('fOuR')).toBe(true);
   });
 });
--- __tests__/quiz.test.js
+++ __tests__/quiz.test.js
@@ -1,24 +1,27 @@
 const Quiz = require('../src/quiz');
 
+let q1 = null;
+let q2 = null;
+let quiz = null;
+
+beforeEach(() => {
+  q1 = { challenge: 'foo', answer: 'bar' };
+  q2 = { challenge: 'baz', answer: 'qux' };
+  quiz = new Quiz('Geography Quiz', [q1, q2]);
+});
+
 describe('Quiz', () => {
   it('has a name', () => {
-    const quiz = new Quiz('Geography Quiz', []);
     expect(quiz.name).toEqual('Geography Quiz');
   });
 
   it('has questions', () => {
-    const q1 = { challenge: 'foo', answer: 'bar' };
-    const q2 = { challenge: 'baz', answer: 'qux' };
-    const quiz = new Quiz('Geography Quiz', [q1, q2]);
     expect(quiz.questions).toEqual([q1, q2]);
   });
 });
 
 describe('readQuestion', () => {
   it('returns the challenge property of the first question', () => {
-    const q1 = { challenge: 'foo', answer: 'bar' };
-    const q2 = { challenge: 'baz', answer: 'qux' };
-    const quiz = new Quiz('Geography Quiz', [q1, q2]);
     expect(quiz.readQuestion()).toEqual(q1.challenge);
   });
 });
